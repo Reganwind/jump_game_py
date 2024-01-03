8b5350297d4bb664542e134d99e9c43c9263e228
zhangsc <zhangsc@asiainfo.com>
2024-01-03 21:13:54 +0800

    first commit

---
+++ main1.py
@@ -0,0 +1,43 @@
+import pygame
+import time
+
+# def size
+SIZE = WIDTH, HEIGHT = 600, 480
+FPS = 10
+
+# def color
+BLACK = 0, 0, 0
+WHITE = 255, 255, 255
+
+# init
+pygame.init()
+pygame.mixer.init()
+
+# init screen
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Game")
+clock = pygame.time.Clock()
+
+# enter
+running = True
+while running:
+
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # update
+    font = pygame.font.SysFont('Menlo', 48, True)
+    current_time = font.render(time.strftime("%Y:%m:%d %H:%M:%S", time.localtime(time.time())), 1, WHITE)
+
+    # draw
+    screen.fill(BLACK)
+    screen.blit(current_time, current_time.get_rect(center=(WIDTH/2, HEIGHT/2)))
+
+    # reflesh
+    pygame.display.update()
+
+# out
+pygame.quit()
