1be45aeee76b8b8019299fd457ce3a6858ebe0b4
zhangsc <zhangsc@asiainfo.com>
2024-01-09 22:57:51 +0800

    --新增死亡重新开始 --新增计分

--- setting.py
+++ setting.py
@@ -2,13 +2,15 @@
 TITLE = "Jumpy"
 WIDTH = 480
 HEIGHT = 320
-FPS = 30
+FPS = 60  ##调整游戏速度
 SIZE = WIDTH, HEIGHT
+FONT_NAME = 'arial'
 
 # Player properties
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.8
+PLAYER_JUMP = 20
 
 # Starting platforms
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
@@ -18,7 +20,7 @@ PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
                  (175, 100, 50, 20)]
 
 #define colors
-WITHE = (255, 255, 255)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
